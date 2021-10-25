2256fc0e2e58a3cc171fca50f82fcdc356ba875d
Fraser Smith <fraser.n.smith@gmail.com.com>
2021-10-25 09:39:03 +0100

    IMS1-14 #comment added read all functionality

--- src/main/java/com/qa/ims/persistence/dao/OrderDAO.java
+++ src/main/java/com/qa/ims/persistence/dao/OrderDAO.java
@@ -1,22 +1,54 @@
 package com.qa.ims.persistence.dao;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.List;
 
-import com.qa.ims.persistence.domain.Order;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
-public class OrderDAO implements Dao<Order>{
+import com.qa.ims.persistence.domain.Order;
+import com.qa.ims.utils.DBUtils;
+
+public class OrderDAO implements Dao<Order> {
+
+	public static final Logger LOGGER = LogManager.getLogger();
 
 	@Override
 	public List<Order> readAll() {
-		
-		return null;
+		try (Connection connection = DBUtils.getInstance().getConnection();
+				Statement statement = connection.createStatement();
+				ResultSet resultSet = statement.executeQuery("SELECT * FROM orders");) {
+			List<Order> orders = new ArrayList<>();
+			while (resultSet.next()) {
+				orders.add(modelFromResultSet(resultSet));
+			}
+			return orders;
+		} catch (SQLException e) {
+			LOGGER.debug(e);
+			LOGGER.error(e.getMessage());
+		}
+		return new ArrayList<>();
 	}
 
 	@Override
 	public Order read(Long id) {
-		// TODO Auto-generated method stub
+		try {
+			Connection connection = DBUtils.getInstance().getConnection();
+			PreparedStatement statement = connection.prepareStatement("SELECT * FROM orders WHERE orderId = ?");
+			statement.setLong(1, id);
+			ResultSet resultSet = statement.executeQuery();
+			resultSet.next();
+			return modelFromResultSet(resultSet);
+		} catch (SQLException e) {
+			LOGGER.debug(e);
+			LOGGER.error(e.getMessage());
+
+		}
 		return null;
 	}
 
